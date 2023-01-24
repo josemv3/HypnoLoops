bebfca0167dd59a497cd6d6998b1c0fb4920a231
Joey Rubin <josemv3@mac.com>
2023-01-23 22:41:29 -0800

    UI for recording page in place for testing.

--- Hypno Loops/ViewControllers/CreateAccountController.swift
+++ Hypno Loops/ViewControllers/CreateAccountController.swift
@@ -12,23 +12,143 @@ class CreateAccountController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
     @IBOutlet weak var usernameErrorLabel: UILabel!
     @IBOutlet weak var emailErrorLabel: UILabel!
     @IBOutlet weak var passwordErrorLabel: UILabel!
+    
     @IBOutlet weak var submitButtton: UIButton!
     @IBOutlet weak var skipButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetForm()
+    }
+    
+    func resetForm() {
+        usernameErrorLabel.isHidden = false
+        emailErrorLabel.isHidden = false
+        passwordErrorLabel.isHidden = false
+       
+        usernameErrorLabel.text = "Required"
+        emailErrorLabel.text = "Required"
+        passwordErrorLabel.text = "Required"
+        
+        usernameTextField.text = ""
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
 
     @IBAction func submitButtonPressed(_ sender: UIButton) {
+        resetForm()
     }
     
     @IBAction func skipButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "gotoProfile", sender: self)
     }
+    
+    //MARK: - Check Username
+    
+    @IBAction func usernameChanged(_ sender: UITextField) {
+        if let username =  usernameTextField.text {
+            if let errorMessage = invalidUsername(username) {
+                usernameErrorLabel.text = errorMessage
+                usernameErrorLabel.isHidden = false
+            } else {
+                usernameErrorLabel.isHidden = true
+            }
+        }
+        checkForValidForm()
+    }
+    
+    func invalidUsername(_ value: String) -> String? {
+        if value.count < 3 {
+            return "Username must have at least 3 characters"
+        }
+        return nil
+    }
+    
+    //MARK: - Check Email
+    
+    @IBAction func emailChanged(_ sender: UITextField) {
+      
+        if let email = emailTextField.text {
+            if let errorMessage = invalidEmail(email) {
+                emailErrorLabel.text = errorMessage
+                emailErrorLabel.isHidden = false
+            } else {
+                emailErrorLabel.isHidden = true
+            }
+        }
+        checkForValidForm()
+    }
+    
+    func invalidEmail(_ value: String) -> String? {
+        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        if !predicate.evaluate(with: value) {
+            return "Invalid Email Address"
+        }
+        return nil
+    }
+    
+    //MARK: - Check Password
+    
+    @IBAction func passwordChanged(_ sender: UITextField) {
+        if let password = passwordTextField.text {
+            if let errorMessage = invalidPassword(password) {
+                passwordErrorLabel.text = errorMessage
+                passwordErrorLabel.isHidden = false
+            } else {
+                passwordErrorLabel.isHidden = true
+            }
+        }
+        checkForValidForm()
+    }
+    
+    func invalidPassword(_ value: String) -> String? {
+        if value.count < 8 {
+            return "Password must be at least 8 characters"
+        }
+        if containsDigit(value) {
+            return "Password must conttain at least 1 digit"
+        }
+        if containsLowercase(value) {
+            return "Password must contain at least 1 lowercase letter"
+        }
+        if containsUppercase(value) {
+            return "Password must contain at least 1 uppercase letter"
+        }
+        return nil
+    }
+    
+    func containsDigit(_ value: String) -> Bool {
+        let regularExpression = ".*[0-9]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func containsLowercase(_ value: String) -> Bool {
+        let regularExpression = ".*[a-z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func containsUppercase(_ value: String) -> Bool {
+        let regularExpression = ".*[A-Z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func checkForValidForm() {
+        if usernameErrorLabel.isHidden && emailErrorLabel.isHidden && passwordErrorLabel.isHidden {
+            submitButtton.isEnabled =  true
+        } else {
+            submitButtton.isEnabled = false
+        }
+    }
+    
+    //MARK: - Seque
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "gotoProfile" {
--- Hypno Loops/ViewControllers/LoopRecordViewController.swift
+++ Hypno Loops/ViewControllers/LoopRecordViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class LoopRecordViewController: UIViewController {
 
+    @IBOutlet weak var affirmationNameLabel: UILabel!
+    @IBOutlet weak var affirmationNameTextField: UITextField!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var micBackgroundView: UIView!
+    @IBOutlet weak var micImageView: UIImageView!
+    
+    @IBOutlet weak var controlsBackgroundView: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var reverbSlider: UISlider!
+    @IBOutlet weak var compressionSlider: UISlider!
+    
+    @IBOutlet weak var saveButtton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        micBackgroundView.layer.cornerRadius = 10
+        controlsBackgroundView.layer.cornerRadius = 10
+        //recordButton.layer.cornerRadius = 0.5 * recordButton.bounds.size.width
+        //playButton.layer.cornerRadius = 0.5 * playButton.bounds.size.width
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recordButtonPressed(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+    }
 }
