91192a6a8b403ab1645a0070a7e79c0158b86b43
Joey Rubin <josemv3@mac.com>
2023-01-24 21:36:41 -0800

    Added profile photo picker.

--- Hypno Loops/ViewControllers/UserProfileController.swift
+++ Hypno Loops/ViewControllers/UserProfileController.swift
@@ -28,12 +28,62 @@ class UserProfileController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = UIColor.darkGray.cgColor
+        //profileImageView.layer.cornerRadius = 0.5 * profileImageView.bounds.size.width
+    }
+    
+    @IBAction func changeProfileImageButtonPushed(_ sender: UIButton) {
+        presentPhotoActionSheet()
     }
     
     @IBAction func doneButtonPushed(_ sender: UIButton) {
         performSegue(withIdentifier: "gotoLoopCollections", sender: self)
+    }
+    
+}
+
+extension UserProfileController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func presentPhotoActionSheet() {
+        let actionSheet = UIAlertController(title: "Profile picture", message: "How would you like to select a profile photo", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionSheet.addAction(UIAlertAction(title: "Take photo", style: .default, handler: {[weak self] _ in
+            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Chose photo", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
         
-        
+        present(actionSheet, animated: true)
+    }
+    
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true //Allows crop box
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true 
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        profileImageView.image = selectedImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
 }
