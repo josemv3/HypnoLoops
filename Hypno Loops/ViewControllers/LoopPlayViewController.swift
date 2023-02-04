d3bdc252826ab2f6e656b34a7eaa441d0557a21b
Olijujuan Green <nihongojaydee@gmail.com>
2023-02-03 21:19:08 -0500

    record and playback functionality

--- Hypno Loops/ViewControllers/LoopPlayViewController.swift
+++ Hypno Loops/ViewControllers/LoopPlayViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LoopPlayViewController: UIViewController {
+    let reverb = AVAudioUnitReverb()
+    let audioEngine = AVAudioEngine()
+    let playerNode = AVAudioPlayerNode()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func setupAudio() {
+        audioEngine.attach(reverb)
+        audioEngine.connect(playerNode, to: reverb, format: nil)
+        audioEngine.connect(reverb, to: audioEngine.outputNode, format: nil)
+        
+        do {
+            try audioEngine.start()
+            playerNode.play()
+        } catch {
+            
+        }
     }
 
 }
+
+
+
--- Hypno Loops/ViewControllers/LoopRecordViewController.swift
+++ Hypno Loops/ViewControllers/LoopRecordViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import AVFoundation
 
-class LoopRecordViewController: UIViewController {
-
+class LoopRecordViewController: UIViewController, AVAudioRecorderDelegate {
+    
     @IBOutlet weak var affirmationNameLabel: UILabel!
     @IBOutlet weak var affirmationNameTextField: UITextField!
     @IBOutlet weak var categoryLabel: UILabel!
@@ -27,6 +28,10 @@ class LoopRecordViewController: UIViewController {
     
     @IBOutlet weak var saveButtton: UIButton!
     
+    var audioRecorder: AVAudioRecorder?
+    var recordingSession: AVAudioSession?
+    var audioPlayer: AVAudioPlayer?
+    
     var cornerRadiusModifier: CGFloat = 10
     
     override func viewDidLoad() {
@@ -40,8 +45,78 @@ class LoopRecordViewController: UIViewController {
     }
     
     @IBAction func playButtonPressed(_ sender: UIButton) {
+        
+        let audioFileName = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        do {
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFileName)
+            audioPlayer?.play()
+            
+        } catch {
+//            handle failure to play recording
+        }
+        
+        
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
+        
+        guard let affirmationName = affirmationNameTextField.text, !affirmationName.isEmpty, let category = categoryTextField.text, !category.isEmpty else {
+//            show error message
+            return
+        }
+        
+//        save recording
+        
     }
+    
+    
+    
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            
+        ]
+        
+        do {
+            
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder?.delegate = self
+            audioRecorder?.record()
+            
+            recordButton.setTitle("Finish Recording", for: .normal)
+            playButton.isEnabled = false
+            
+        } catch {
+            
+        }
+        
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder?.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Start Recording", for: .normal)
+            playButton.isEnabled = true
+        } else {
+            recordButton.setTitle("Start Recording", for: .normal)
+        }
+        
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
 }
