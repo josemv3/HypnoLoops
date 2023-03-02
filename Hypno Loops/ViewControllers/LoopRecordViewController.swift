d73555ef214fa90313dbfa7bfb8f0d046be213e7
Olijujuan Green <nihongojaydee@gmail.com>
2023-03-02 17:41:05 -0500

    play functionality implemented

--- Hypno Loops/ViewControllers/LoopRecordViewController.swift
+++ Hypno Loops/ViewControllers/LoopRecordViewController.swift
@@ -29,16 +29,17 @@ class LoopRecordViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet weak var saveButtton: UIButton!
     
     var audioRecorder: AVAudioRecorder?
-    var recordingSession: AVAudioSession?
+    var recordingSession = AVAudioSession.sharedInstance()
     var audioPlayer: AVAudioPlayer?
     
     var isRecording = false
     var isPlaying = false
     
-     
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         micBackgroundView.layer.cornerRadius = CornerRadiusModifiers.normal.size
         micBackgroundView.layer.borderWidth = BorderSize.small.size
         micBackgroundView.layer.borderColor = UIColor(named: Color.hlBlue.rawValue)?.cgColor
@@ -71,45 +72,29 @@ class LoopRecordViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     @IBAction func playButtonPressed(_ sender: UIButton) {
-        
-        let audioFileName = getDocumentsDirectory().appendingPathComponent("recording.m4a")
-        
-        do {
-            
-            let audioEngine = AVAudioEngine()
-            let audioPlayerNode = AVAudioPlayerNode()
-            let reverb = AVAudioUnitReverb()
-            reverb.loadFactoryPreset(.cathedral)
-            reverb.wetDryMix = 50
-            audioEngine.attach(audioPlayerNode)
-            audioEngine.attach(reverb)
-            audioEngine.connect(audioPlayerNode, to: reverb, format: nil)
-            audioEngine.connect(reverb, to: audioEngine.mainMixerNode, format: nil)
-            audioEngine.prepare()
-            try! audioEngine.start()
-            audioPlayerNode.play()
-            
-            audioPlayer = try AVAudioPlayer(contentsOf: audioFileName)
-            audioPlayer?.play()
-            
+        if !isPlaying {
+            isPlaying.toggle()
+            recordButton.isEnabled = false
             let stopImage = UIImage(systemName: "stop.circle.fill")
             playButton.setImage(stopImage, for: .normal)
-            
-        } catch {
-//            handle failure to play recording
+            startPlaying()
+        } else {
+            isPlaying.toggle()
+            recordButton.isEnabled = false
+            let playImage = UIImage(systemName: "play.fill")
+            playButton.setImage(playImage, for: .normal)
+            finishPlaying()
         }
-        
-        
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
         
         guard let affirmationName = affirmationNameTextField.text, !affirmationName.isEmpty, let category = categoryTextField.text, !category.isEmpty else {
-//            show error message
+            //            show error message
             return
         }
         
-//        save recording
+        //        save recording
         
     }
     
@@ -129,16 +114,17 @@ class LoopRecordViewController: UIViewController, AVAudioRecorderDelegate {
         ]
         
         do {
-            
+            try recordingSession.setCategory(.playAndRecord)
+            try recordingSession.setActive(true)
             audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
             audioRecorder?.delegate = self
             audioRecorder?.record()
             
-            recordButton.setTitle("Finish Recording", for: .normal)
+            recordButton.setTitle("Stop", for: .normal)
             playButton.isEnabled = false
             
         } catch {
-            
+            print("Error: \(error.localizedDescription)")
         }
         
     }
@@ -148,11 +134,44 @@ class LoopRecordViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder = nil
         
         if success {
-            recordButton.setTitle("Start Recording", for: .normal)
+            recordButton.setTitle("Record", for: .normal)
             playButton.isEnabled = true
         } else {
-            recordButton.setTitle("Start Recording", for: .normal)
+            recordButton.setTitle("Record", for: .normal)
         }
+    }
+    
+    func startPlaying()  {
+            
+            let audioFileName = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+            
+            do {
+                let audioEngine = AVAudioEngine()
+                let audioPlayerNode = AVAudioPlayerNode()
+                let reverb = AVAudioUnitReverb()
+                reverb.loadFactoryPreset(.cathedral)
+                reverb.wetDryMix = 50
+                audioEngine.attach(audioPlayerNode)
+                audioEngine.attach(reverb)
+                audioEngine.connect(audioPlayerNode, to: reverb, format: nil)
+                audioEngine.connect(reverb, to: audioEngine.mainMixerNode, format: nil)
+                audioEngine.prepare()
+                try! audioEngine.start()
+                audioPlayerNode.play()
+                
+                audioPlayer = try AVAudioPlayer(contentsOf: audioFileName)
+                audioPlayer?.play()
+                
+                let stopImage = UIImage(systemName: "stop.circle.fill")
+                playButton.setImage(stopImage, for: .normal)
+                
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    
+    func finishPlaying() {
+        audioPlayer?.stop()
     }
     
     func getDocumentsDirectory() -> URL {
