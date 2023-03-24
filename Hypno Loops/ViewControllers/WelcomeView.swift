32fe12eef10105374ec6986d70a1359a8d3cc52c
Olijujuan Green <nihongojaydee@gmail.com>
2023-03-23 22:01:29 -0400

    profile photo population implemented

--- Hypno Loops/ViewControllers/WelcomeView.swift
+++ Hypno Loops/ViewControllers/WelcomeView.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import Firebase
 
 class WelcomeView: UIViewController {
-
+    
     @IBOutlet weak var TopLogoView: UIImageView!
     @IBOutlet weak var userLoginButton: UIButton!
     @IBOutlet weak var userLoginImage: UIImageView!
@@ -18,9 +19,34 @@ class WelcomeView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureProfileImageView()
         userLoginImage.layer.borderWidth = BorderSize.small.size
         userLoginImage.layer.cornerRadius = CornerRadiusModifiers.normal.size
         userLoginImage.layer.borderColor = UIColor(named: Color.hlBlue.rawValue)?.cgColor
+    }
+    
+    func configureProfileImageView() {
+        if let user = Auth.auth().currentUser {
+            userLoginButton.isHidden = true
+            let reference = Database.database().reference(fromURL: RealtimeDatabase.referenceURLString.rawValue)
+            let userRef = reference.child("users").child(user.uid)
+            
+            userRef.observeSingleEvent(of: .value) { snapshot in
+                let profilePhotoURL = snapshot.childSnapshot(forPath: "profilePhotoURL").value as! String
+                let url = URL(string: profilePhotoURL)!
+                
+                URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+                    guard let data = data else { return }
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        self?.userLoginImage.image = image
+                    }
+                }.resume()
+            }
+            
+        } else {
+            print("logged out")
+        }
     }
     
     @IBAction func categoryViewButtonPush(_ sender: UIButton) {
