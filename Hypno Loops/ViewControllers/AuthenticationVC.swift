2dfc8f498f444e41d17f47ef3bfc432a88aecacd
Olijujuan Green <olijujuan.green@onepeloton.com>
2023-05-26 20:37:44 -0400

    new authvc and accompanying storyboard

---
+++ Hypno Loops/ViewControllers/AuthenticationVC.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationVC.swift
+//  Hypno Loops
+//
+//  Created by Olijujuan Green on 5/26/23.
+//
+
+import UIKit
+
+class AuthenticationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
